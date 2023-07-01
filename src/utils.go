de659471ecb28f0ab6c637c0512c473909a35b1f
logotipiwe <german.reus@bk.ru>
2023-07-02 03:57:10 +0600

    Init

---
+++ src/utils.go
@@ -0,0 +1,18 @@
+package dc_go_utils
+
+func Map[T, U any](ts []T, f func(T) U) []U {
+	us := make([]U, len(ts))
+	for i := range ts {
+		us[i] = f(ts[i])
+	}
+	return us
+}
+
+func toMap[K string, V any](arr []V, groupingFunc func(val V) K) map[K]V {
+	result := make(map[K]V)
+	for _, v := range arr {
+		key := groupingFunc(v)
+		result[key] = v
+	}
+	return result
+}
