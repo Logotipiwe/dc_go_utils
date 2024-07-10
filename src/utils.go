445f37fc3a069d73635233c698c9eb239ccf5117
logotipiwe <german.reus@bk.ru>
2024-07-10 22:26:16 +0300

    SleepWithInterruption func v1.0.6

--- src/utils.go
+++ src/utils.go
@@ -1,5 +1,7 @@
 package utils
 
+import "time"
+
 func Map[T, U any](ts []T, f func(T) U) []U {
 	us := make([]U, len(ts))
 	for i := range ts {
@@ -15,4 +17,26 @@ func ToMap[K string, V any](arr []V, groupingFunc func(val V) K) map[K]V {
 		result[key] = v
 	}
 	return result
+}
+
+func SleepWithInterruption(getSleepTimeFunc func() time.Duration, checkInterval time.Duration) {
+	sleepFor := getSleepTimeFunc()
+	if sleepFor < checkInterval {
+		time.Sleep(sleepFor)
+	} else {
+		slept := time.Duration(0)
+		for {
+			left := sleepFor - slept
+			if left > checkInterval {
+				time.Sleep(checkInterval)
+				slept += checkInterval
+			} else {
+				if left > 0 {
+					time.Sleep(left)
+				}
+				return
+			}
+			sleepFor = getSleepTimeFunc()
+		}
+	}
 }
